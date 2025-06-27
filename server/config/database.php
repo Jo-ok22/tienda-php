1d84beb9a0b6c13d8916c13e4a51bfc120f66170
Jo-ok22 <lunajosiasm@gmail.com>
2025-06-27 15:42:13 -0300

    :P

--- server/config/database.php
+++ server/config/database.php
@@ -1,22 +1,45 @@
 <?php
+
 // conection = mysql -u root -h localhost -p
+
 class Database {
-    private $host = "localhost";
-    private $username = "root";
-    private $password = "";
-    private $db_name = "tienda_jey";
+    private $host;
+    private $username;
+    private $password;
+    private $db_name;
     public $conn;
+
+    public function __construct() {
+        $this->host = getenv('DB_HOST');
+        $this->username = getenv('DB_USERNAME');
+        $this->password = getenv('DB_PASSWORD');
+        $this->db_name = getenv('DB_NAME');
+    }
 
     public function conectar() {
         $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+
         if ($this->conn->connect_error) {
             die("Error de conexión: " . $this->conn->connect_error);
         }
+
         $this->conn->set_charset("utf8");
         return $this->conn;
     }
 }
-?>
+
+
+
+
+
+
+
+
+
+
+
+
+// conection = mysql -u root -h localhost -p
 
 
 
--- server/vendor/composer/autoload_real.php
+++ server/vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit5b9b04e6045a8fecfaef203ef037f3f6
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit5b9b04e6045a8fecfaef203ef037f3f6::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
